f449f199be7d71bf945637fdb4549e3a97cf9dbd
Sujeet Hiremath <55036008+sujeethiremath@users.noreply.github.com>
2019-10-10 22:33:52 -0700

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,25 @@
+Los Angeles
+San Diego
+San Jose
+San Francisco
+Fresno
+Sacramento
+Long Beach
+Oakland
+Bakersfield
+Portland
+Las Vegas
+Phoenix
+Albuquerque
+Dallas
+San Antonio
+New Orleans
+Orlando
+Atlanta
+Nashville
+Kansas City
+St. Louis
+Chicago
+Minneapolis
+Detroit
+Columbus
